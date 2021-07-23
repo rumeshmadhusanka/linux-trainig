331611579c40f223918c258214afd0a0e21f6409
Rumesh Madhusanka <32504465+rumeshmadhusanka@users.noreply.github.com>
2021-07-23 19:09:14 +0530

    Create myprohram2.py

---
+++ security/myprohram2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import hashlib
+from sys import argv
+import secrets
+
+hash_algo = "SHA512"
+iterations = 200000
+try:
+    assert len(argv) > 1
+    salt = secrets.token_bytes()
+    value = bytearray(argv[1], "utf8")
+    hash_value = hashlib.pbkdf2_hmac(hash_name=hash_algo, password=value, iterations=iterations, salt=salt)
+    print(hash_value.hex())
+except AssertionError:
+    print("Incorrect number of arguments")
+except Exception:
+    print("Exception occurred")
