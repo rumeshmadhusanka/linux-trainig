e9a0239803a4dd6ffe0690b18f21b97e340b7a91
Rumesh Madhusanka <32504465+rumeshmadhusanka@users.noreply.github.com>
2021-07-23 19:14:55 +0530

    Update myprogram2.py

--- security/myprogram2.py
+++ security/myprogram2.py
@@ -7,7 +7,7 @@ hash_algo = "SHA512"
 iterations = 200000
 try:
     assert len(argv) > 1
-    salt = secrets.token_bytes()
+    salt = secrets.token_bytes(128)
     value = bytearray(argv[1], "utf8")
     hash_value = hashlib.pbkdf2_hmac(hash_name=hash_algo, password=value, iterations=iterations, salt=salt)
     print(hash_value.hex())
